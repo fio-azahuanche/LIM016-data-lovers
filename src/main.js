9ef41bcfd1d9f9df5b2435fe2c763e44b8f8d19b
unknown <cruzyumari2@gmail.com>
2021-10-28 23:52:53 -0500

    Intento 1 de importar data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+const fila = document.querySelector('.contenedorCarrusel');
 
-console.log(example, data);
+const flechaIzquierda = document.getElementById('flechaIzquierda');
+flechaIzquierda.addEventListener('click', ()=>{
+  fila.scrollLeft -= fila.offsetWidth;
+});
+
+const flechaDerecha = document.getElementById('flechaDerecha');
+flechaDerecha.addEventListener('click', ()=>{
+  fila.scrollLeft += fila.offsetWidth;
+});
+
+document.body.onload=addElement;
+function addElement () {
+    const img=document.getElementById("poster1");
+    let Poster1import=data.films[0].poster;
+
+    
+    img.src=Poster1import;
+    document.body.appendChild(img);
+    var currentDiv = document.getElementById("poster1");
+  document.body.insertBefore(img, currentDiv);
+}
+
+console.log(data.films[0].title);
+
+//console.log(example, data);
