f19bca712596c7fa4e1d3a8f7e6c821e47cd37ba
unknown <fazahuanchef@gmail.com>
2021-10-30 00:07:15 -0500

    ordenando el código y agregando comentarios

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 //import { example } from './data.js';
-// import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
+/* Evento para mostrar y quitar las secciones Peliculas y Principal*/
 let nav=document.getElementsByClassName("enlaces");
 nav[0].addEventListener('click',()=>{
   document.getElementById("Principal").style.display="none";
   document.getElementById("Peliculas").style.display="block";
 })
 
-
-
+/* Evento para crear carrusel de peliculas populares*/
 const fila = document.querySelector('.contenedorCarrusel');
-
 const flechaIzquierda = document.getElementById('flechaIzquierda');
 flechaIzquierda.addEventListener('click', ()=>{
   fila.scrollLeft -= fila.offsetWidth;
@@ -23,20 +20,21 @@ flechaDerecha.addEventListener('click', ()=>{
   fila.scrollLeft += fila.offsetWidth;
 });
 
-//Peliculas
+/* Mostrando Posters y año de las peliculas*/
 let div=[];
 let img=[];
 let year=[];
 
 data.films.forEach((film,index)=>{
   div[index]=document.createElement("div");
-    img[index]=document.createElement("img");
-    year[index]=document.createElement("span")
-    img[index].src=film.poster;
-    year[index].textContent=film.release_date;
-    posters.appendChild(div[index]);
-    div[index].appendChild(img[index]);
-    div[index].appendChild(year[index]);
-    div[index].classList.add("infoPeliculas");
+  img[index]=document.createElement("img");
+  year[index]=document.createElement("span")
+  img[index].src=film.poster;
+  year[index].textContent=film.release_date;
+  posters.appendChild(div[index]);
+  div[index].appendChild(img[index]);
+  div[index].appendChild(year[index]);
+  div[index].classList.add("infoPeliculas");
 })
-//Comentario para ver si funciona el pull request
+
+//Comentario para probar por segunda vez el pull request
