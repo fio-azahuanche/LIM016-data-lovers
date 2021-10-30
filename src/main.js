a5fc0c3a59186dae3dfba4ed9e3ecc42fa1822a3
unknown <cruzyumari2@gmail.com>
2021-10-30 12:06:09 -0500

    Entrar a la seccion Personajes sin importar data

--- src/main.js
+++ src/main.js
@@ -21,20 +21,42 @@ flechaDerecha.addEventListener('click', ()=>{
 });
 
 /* Mostrando Posters y año de las peliculas*/
-let div=[];
-let img=[];
-let year=[];
+let div=[], img=[], year=[], score=[],etiqueta=[], star=[];
 
 data.films.forEach((film,index)=>{
   div[index]=document.createElement("div");
   img[index]=document.createElement("img");
+  etiqueta[index]=document.createElement("div");
   year[index]=document.createElement("span")
+  score[index]=document.createElement("span");
+  star[index]=document.createElement("img");
+
+
   img[index].src=film.poster;
   year[index].textContent=film.release_date;
+  score[index].textContent=film.rt_score;
+  star[index].src="bxs-star 1.png";
+
+
   posters.appendChild(div[index]);
   div[index].appendChild(img[index]);
-  div[index].appendChild(year[index]);
+  div[index].appendChild(etiqueta[index]);
+  etiqueta[index].appendChild(year[index]);
+  etiqueta[index].appendChild(score[index]);
+  score[index].appendChild(star[index]);
+
+
   div[index].classList.add("infoPeliculas");
+  etiqueta[index].classList.add("etiquetas");
+  img[index].classList.add("imgPosters");
+
+
+  img[index].addEventListener('click',()=>{
+    document.getElementById("Peliculas").style.display="none";
+    document.getElementById("SectionInfoPeliculas").style.display="block";
+  })
+
+
 })
 
 //Comentario para probar por segunda vez el pull request
