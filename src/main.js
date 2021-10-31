cfb9145a24507c1baab72d0618f4fcde98053bb0
unknown <cruzyumari2@gmail.com>
2021-10-30 21:55:53 -0500

    Creando titulos sobre posters de las peliculas

--- src/main.js
+++ src/main.js
@@ -4,17 +4,19 @@ import data from './data/ghibli/ghibli.js';
 /* Event to show Films and remove Principal*/
 let nav=document.getElementsByClassName("links");
 nav[0].addEventListener('click',()=>{
-  document.getElementById("Principal").style.display="none";
+  document.getElementById("Home").style.display="none";
   document.getElementById("Films").style.display="block";
 });
 
 /* Event to show Films and remove Principal, filInfoSections*/
-const buttonBackFilms = document.getElementById("buttonBackFilms");
+let buttonBackFilms = document.getElementById("buttonBackFilms");
 buttonBackFilms.addEventListener('click', ()=>{
-  document.getElementById("Principal").style.display="none";
+  document.getElementById("Home").style.display="none";
   document.getElementById("filmInfoSection").style.display="none";
   document.getElementById("Films").style.display="block";
 });
+
+
 
 /* Event to create carousel of popular movies*/
 const row = document.querySelector('.containerCarousel');
@@ -29,7 +31,7 @@ rightArrow.addEventListener('click', ()=>{
 });
 
 /* Showing Posters and year of the films*/
-let div=[], img=[], year=[], score=[],tag=[], star=[];
+var div=[], img=[], year=[], score=[],tag=[], star=[], pantallanegra=[],txt=[];
 
 data.films.forEach((film,index)=>{
   div[index]=document.createElement("div");
@@ -38,33 +40,38 @@ data.films.forEach((film,index)=>{
   year[index]=document.createElement("span")
   score[index]=document.createElement("span");
   star[index]=document.createElement("img");
-
+  //
+  pantallanegra[index]=document.createElement("div");
+  txt[index]=document.createElement("div");
+//
 
   img[index].src=film.poster;
   year[index].textContent=film.release_date;
   score[index].textContent=film.rt_score;
   star[index].src="bxs-star 1.png";
-
+//
+txt[index].textContent=film.title;
+//
 
   posters.appendChild(div[index]);
   div[index].appendChild(img[index]);
-  div[index].appendChild(tag[index]);
+ div[index].appendChild(tag[index]);
   tag[index].appendChild(year[index]);
   tag[index].appendChild(score[index]);
   score[index].appendChild(star[index]);
+  //
+  div[index].appendChild(pantallanegra[index]);
+  pantallanegra[index].appendChild(txt[index]);
+  //
 
 
   div[index].classList.add("infoMovies");
   tag[index].classList.add("tags");
   img[index].classList.add("imgPosters");
-
-
-  img[index].addEventListener('click',()=>{
-    document.getElementById("Films").style.display="none";
-    document.getElementById("filmInfoSection").style.display="block";
-  })
-
+  //
+  pantallanegra[index].classList.add("overlay");
+  txt[index].classList.add("txtCard");
+  //
 
 })
 
-//Comentario para probar por segunda vez el pull request
