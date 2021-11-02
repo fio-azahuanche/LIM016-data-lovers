ace6d533eda08f04414ca52d739ee96deeb8db11
unknown <fazahuanchef@gmail.com>
2021-11-02 02:11:52 -0500

    ordenando poster s por año y score

--- src/data.js
+++ src/data.js
@@ -1,7 +1,25 @@
 // estas funciones son de ejemplo
 
-export const filterDataByDirectorProducer = (data, condition) => {
+export const filterByDirectorProducer = (data, condition) => {
   return data.filter(film => film.director === condition || film.producer === condition);
+};
+
+export const sortScore = (data, items) => {
+  //sort score in descending order (highest to lowest)
+  const score = data.sort((a, b) => (parseInt(b[items]) - parseInt(a[items])));
+  return score;
+};
+
+export const sortYearDesc = (data, items) => {
+  //sort year in descending order (highest to lowest)
+  const yearDesc = data.sort((a, b) => (parseInt(b[items]) - parseInt(a[items])));
+  return yearDesc;
+};
+
+export const sortYearAsc = (data, items) => {
+  //sort year in ascending order (lowest to highest)
+  const yearAsc = data.sort((a, b) => (parseInt(a[items]) - parseInt(b[items])));
+  return yearAsc;
 };
 
 export const anotherExample = () => {
