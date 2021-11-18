24b9aa21a54c36a03799d43eb6430818b3bbd44f
unknown <cruzyumari2@gmail.com>
2021-11-18 11:45:19 -0500

    Uso del fetch y casi terminado

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
-import { filterByDirectorProducer, sortData, searchData, top10Movies } from './data.js';
-import data from './data/ghibli/ghibli.js';
+import { filterByDirectorProducer, sortData, searchData } from './data.js';
 
 const getById = label => document.getElementById(label);
 const getByClass = label => document.getElementsByClassName(label);
 
-const dataFilms=data.films;
+let dataFilms;
+fetch('./data/ghibli/ghibli.json')
+  .then(studio => studio.json())//establece la conexión con el archivo
+  .then(data => {
+    dataFilms = data.films; //llama a la data
+  })
 
 let home = getById("Home");
 let nav = getByClass("links");
@@ -25,7 +29,6 @@ let listProducer = getById("listProducer");
 let character = getById("character");
 let locations = getById("locations");
 let vehicles = getById("vehicles");
-//let cardCharacter = getById("cardEachCharacter");
 let cardModal = getById("cardModal");
 let Statistics = getById("Statistics");
 
@@ -111,13 +114,7 @@ const enterCardChar = (group, container, className) => {
 const card = (element, item) => {
   getById("dni").innerHTML = "";
   getById("cardEachCharacter1").innerHTML = "";
-  //cardCharacter.innerHTML = "";
-  /* let bttnCloseModal = document.createElement("button");
-  bttnCloseModal.innerHTML = `<img src="pictures/x-bold 1.png">`;
-  bttnCloseModal.setAttribute('id', 'closeModal'); */
   getById("cardEachCharacter1").innerHTML=`<h1>${element.name}</h1><hr>`
-  /* let name=document.createElement("h1");
-  name.innerHTML=element.name; */
 
   let generalDiv = document.createElement("div");
   generalDiv.classList.add("flex");
@@ -165,23 +162,18 @@ const card = (element, item) => {
   getById("dni").textContent="ID: "+element.id.slice(0,8);
   generalDiv.appendChild(divImgCard);
   generalDiv.appendChild(divDetallesGrid);
-  //getById("cardEachCharacter1").appendChild(name);
   getById("cardEachCharacter1").appendChild(generalDiv);
-  //cardCharacter.appendChild(generalDiv);
-  /* getById("closeModal").addEventListener('click', () => {
-    cardModal.style.display = "none";
-  }) */
+  
 };
 getById("closeModal").addEventListener('click', () => {
   cardModal.style.display = "none";
 })
 
 const dataNueva = (clase) => {
-  let idS = [];
+  let idS = [],array = [];
   for (let i = 0; i < clase.length; i++) {
     idS[i] = clase[i].getAttribute('id');
   }
-  let array = [];
   for (let i = 0; i < idS.length; i++) {
     array[i] = dataFilms.filter(film => film.title == idS[i])[0];
   }
@@ -368,84 +360,4 @@ getById("logoBackHome2").addEventListener('click', () => {
 navStatistics.addEventListener('click', () => {
   home.style.display = "none";
   Statistics.style.display = "block";
-});
-
-const addChart = (title, typeChart, labelsChart, legendShow, string)=>{
-  const chart = getById(title).getContext('2d');
-  const colors = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'orange','#C32BAD','#7027A0','#6ECB63','#3A6351','#1E3163', '#C84B31'];
-  //eslint-disable-next-line
-  const charts = new Chart(chart,{
-    type: typeChart,
-    data: {
-      labels: labelsChart[0],
-      datasets: [{
-        label: string,
-        data: labelsChart[1],
-        backgroundColor: colors,
-        borderColor: [
-          'rgba(255, 99, 132, 1)'
-        ],
-        borderWidth: 1.5,
-        hoverOffset: 4
-      }],
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    },
-    options: {
-      plugins: {
-        title: {
-          display: true,
-          text: title.toUpperCase(),
-          font: {
-            size: 30
-          }
-        },
-        legend: {
-          position: 'right',
-          display: legendShow
-        }
-      },
-      responsive: true,
-    }
-  })
-}
-
-const dataLabelToChart = (data, topic, labels) => {
-  let dataChart = [];
-  for (let i = 0; i < data.length; i++) {
-    dataChart[i] = data[i].people.map(item => item[topic]);
-  }
-  let arrayLabel=labels;
-  let arrayConcat = dataChart.reduce((a, b) => a.concat(b));
-  let amountByLabel = [];
-  for (let i = 0; i < arrayLabel.length - 1; i++) {
-    amountByLabel[i] = arrayConcat.filter(item => item == arrayLabel[i]).length;
-  }
-  let sum = amountByLabel.reduce((a, b) => a + b);
-  amountByLabel[arrayLabel.length - 1] = arrayConcat.length - sum;
-  return [arrayLabel, amountByLabel]
-}
-
-const genresMovie = (data) => {
-  let genres = data.map(item => item.genres);
-  let arrayGenres = [], countGenres = [];
-  genres.forEach((item,index)=>arrayGenres[index] = item.split(" "));
-  let genresConcat = arrayGenres.reduce((a, b) => a.concat(b));
-  let types = genresConcat.filter((item, index) => genresConcat.indexOf(item) === index);
-  types.forEach((element,index)=>countGenres[index] = genresConcat.filter(item => item == element).length);
-  return [types, countGenres];
-}
-
-document.addEventListener('DOMContentLoaded', ()=>{
-  let species = ['Human', 'Cat', 'Raccoon Dog', 'Spirit', 'Deity, Dragon', 'Totoro', 'Witch', 'Borrower', 'Others'];
-  let gender = ['Female', 'Male', 'Others'];
-  addChart('gender', 'pie', dataLabelToChart(dataFilms, 'gender', gender),true, " amount");
-  addChart('specie', 'doughnut', dataLabelToChart(dataFilms, 'specie', species),true, " amount");
-  addChart('top10', 'line', top10Movies(dataFilms),false, " score");
-  addChart('Genres', 'bar', genresMovie(dataFilms),false, " amount");
 });
---
+++ src/statistics.js
@@ -0,0 +1,83 @@
+import {  top10Movies } from './data.js';
+import data from './data/ghibli/ghibli.js';
+let dataFilms=data.films;
+const getById = label => document.getElementById(label);
+const addChart = (title, typeChart, labelsChart, legendShow, string)=>{
+    const chart = getById(title).getContext('2d');
+    const colors = ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'orange','#C32BAD','#7027A0','#6ECB63','#3A6351','#1E3163', '#C84B31'];
+    //eslint-disable-next-line
+    const charts = new Chart(chart,{
+      type: typeChart,
+      data: {
+        labels: labelsChart[0],
+        datasets: [{
+          label: string,
+          data: labelsChart[1],
+          backgroundColor: colors,
+          borderColor: [
+            'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1.5,
+          hoverOffset: 4
+        }],
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      },
+      options: {
+        plugins: {
+          title: {
+            display: true,
+            text: title.toUpperCase(),
+            font: {
+              size: 30
+            }
+          },
+          legend: {
+            position: 'right',
+            display: legendShow
+          }
+        },
+        responsive: true,
+      }
+    })
+  }
+  
+  const dataLabelToChart = (data, topic, labels) => {
+    let dataChart = [];
+    for (let i = 0; i < data.length; i++) {
+      dataChart[i] = data[i].people.map(item => item[topic]);
+    }
+    let arrayLabel=labels;
+    let arrayConcat = dataChart.reduce((a, b) => a.concat(b));
+    let amountByLabel = [];
+    for (let i = 0; i < arrayLabel.length - 1; i++) {
+      amountByLabel[i] = arrayConcat.filter(item => item == arrayLabel[i]).length;
+    }
+    let sum = amountByLabel.reduce((a, b) => a + b);
+    amountByLabel[arrayLabel.length - 1] = arrayConcat.length - sum;
+    return [arrayLabel, amountByLabel]
+  }
+  
+  const genresMovie = (data) => {
+    let genres = data.map(item => item.genres);
+    let arrayGenres = [], countGenres = [];
+    genres.forEach((item,index)=>arrayGenres[index] = item.split(" "));
+    let genresConcat = arrayGenres.reduce((a, b) => a.concat(b));
+    let types = genresConcat.filter((item, index) => genresConcat.indexOf(item) === index);
+    types.forEach((element,index)=>countGenres[index] = genresConcat.filter(item => item == element).length);
+    return [types, countGenres];
+  }
+  
+  document.addEventListener('DOMContentLoaded', ()=>{
+    let species = ['Human', 'Cat', 'Raccoon Dog', 'Spirit', 'Deity, Dragon', 'Totoro', 'Witch', 'Borrower', 'Others'];
+    let gender = ['Female', 'Male', 'Others'];
+    addChart('gender', 'pie', dataLabelToChart(dataFilms, 'gender', gender),true, " amount");
+    addChart('specie', 'doughnut', dataLabelToChart(dataFilms, 'specie', species),true, " amount");
+    addChart('top10', 'line', top10Movies(dataFilms),false, " score");
+    addChart('Genres', 'bar', genresMovie(dataFilms),false, " amount");
+  });
